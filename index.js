06990df609bee17facaf36821b44e565d6cc96ec
Romain Séguy <rom.seguy@gmail.com>
2015-08-19 01:24:08 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { forceGraph } from 'd3-state-visualizer'
+import d3 from 'd3';
+import Chart from './components/Chart';
+
+const appState = {
+  foo: ['1', '2', '3'],
+  bar: 3,
+  baz: { some: 'value' },
+  todo: ['a', 'b']
+};
+
+const nodes = Object.keys(appState).map(key => ({
+  key,
+  [key]: appState[key]
+}));
+
+const initialize = forceGraph();
+
+React.render(
+  <Chart
+    initialize={initialize}
+    data={nodes}
+    id='forceGraphReactExample'
+    charge={-50}
+    linkDistance={80}
+    />
+  , document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    }]
+  }
+};
