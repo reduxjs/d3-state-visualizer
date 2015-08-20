ffd8854307c856eda5c7b1d3189326288ddcaf0d
Romain Séguy <rom.seguy@gmail.com>
2015-08-20 22:33:56 +0200

    tree example

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-import { forceGraph } from 'd3-state-visualizer'
+import { tree } from 'd3-state-visualizer';
 import d3 from 'd3';
 
 const appState = {
-  foo: ['1', '2', '3'],
-  bar: 3,
-  baz: { some: 'value' },
-  todo: ['a', 'b']
+  todoStore: {
+    todos: [
+      { title: 'd3'},
+      { title: 'state' },
+      { title: 'visualizer' },
+      { title: 'tree' }
+    ],
+    completedCount: 1,
+    alphabeticalOrder: true
+  },
+  someStore: {
+    someProperty: 0,
+    someObject: {
+      anotherProperty: 'value',
+      someArray: [0, 1, 2]
+    }
+  }
 };
 
-const nodes = Object.keys(appState).map(key => ({
-  key,
-  [key]: appState[key]
-}));
+const initialize = tree();
 
-const initialize = forceGraph();
-
-const root = document.getElementById('root');
-
-const render = initialize(d3, root, {
-  data: nodes,
-  id: 'forceGraphExample',
+const render = initialize(d3, document.getElementById('root'), {
+  state: appState,
+  id: 'treeExample',
   size: 1000,
-  aspectRatio: 1,
-  charge: -30,
-  linkDistance: 100,
-  maxNodeSize: 50
+  aspectRatio: 0.5,
+  isSorted: false,
+  widthBetweenBranchCoeff: 2,
+  heightBetweenNodesCoeff: 1.5,
+  style: 'border: 1px solid black'
 });
 
 render();
-
-document.getElementById('addNodesButton').addEventListener('click', () => {
-  render([{space: -1}, {invaders: 4}])
-});
