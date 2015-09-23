2c65ae4568aa9f00ed2188e2b2c24215bd0127f2
Romain Séguy <rom.seguy@gmail.com>
2015-09-23 17:26:02 +0200

    Add zooming/panning

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { tree } from 'd3-state-visualizer';
+import visualizer from 'd3-state-visualizer';
 import ChartContainer from './components/ChartContainer';
-import Chart from './components/Chart';
 
 const appState = {
   todoStore: {
@@ -23,21 +22,43 @@ const appState = {
   }
 };
 
-const initialize = tree();
+const { components: { TreeChart }} = visualizer;
+
+const style = {
+  border: '1px solid black',
+  link: {
+    'stroke-width': '0.5px'
+  }
+};
+
+const tooltipOptions = {
+  style: {
+    'font-family': 'Consolas, Menlo, Monaco, monospace',
+    'font-size': '0.8em',
+    color: 'white',
+    background: 'rgba(0, 0, 0, 0.5)',
+    'border-radius': '5px',
+    padding: '10px'
+  },
+  offset: {
+    top: -390,
+    left: 30
+  }
+}
 
 React.render(
   <div>
     <ChartContainer appState={appState} >
-      <Chart
-        initialize={initialize}
-        state={appState}
+      <TreeChart
         id='treeExample'
-        size={1000}
+        size={619}
         aspectRatio={0.5}
         isSorted={true}
         transitionDuration={0}
         widthBetweenBranchCoeff={2}
         heightBetweenNodesCoeff={1.5}
+        style={style}
+        tooltipOptions={tooltipOptions}
         />
     </ChartContainer>
   </div>
